8c936aa7cf31a750ae7d3786737448c7ff24400b
jonatan <90124081@unitedtractors.com>
2025-11-29 10:51:00 +0700

    ♻️ : Restructure app routing and add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            'star-movement-bottom': 'star-movement-bottom linear infinite alternate',
+            'star-movement-top': 'star-movement-top linear infinite alternate',
+            keyframes: {
+                'star-movement-bottom': {
+                '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+                '100%': { transform: 'translate(-100%, 0%)', opacity: '0' },
+                },
+                'star-movement-top': {
+                '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+                '100%': { transform: 'translate(100%, 0%)', opacity: '0' },
+                },
+            },
+            colors: {
+                primary: "#F4B400",
+                dark: "#1E232C",
+                milk: "#EFEFEF",
+            }
+        },
+    },
+    plugins: [],
+}
